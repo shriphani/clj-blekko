fa9e28f8baff51c6ff0c4f259500450870a6d948
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-16 14:47:28 -0500

    added rate-limiting

--- src/clj_blekko/core.clj
+++ src/clj_blekko/core.clj
@@ -1,6 +1,7 @@
 (ns clj-blekko.core
   (:require [cheshire.core :as json]
             [clj-time.core :as time]
+            [clj-time.coerce :as coerce]
             [clj-http.client :as client]
             (org.bovinegenius [exploding-fish :as uri])))
 
@@ -8,9 +9,7 @@
 
 (def *last-request* (atom nil))
 
-(defn wait-a-sec
-  []
-  (let [cur-time (time/now)]))
+(def *delay-secs* 1)
 
 (defn run-query
   "Rate limited to 1 query per second"
@@ -23,20 +22,25 @@
            encoded-query (if (:json options-map)
                            (str q " /json /ps=100")
                            (str q " /ps=100"))
-           query-url     (-> *url-base*
-                             (uri/param "q" encoded-query)
-                             (uri/param "auth" auth))
-
-           cur-time      (time/now)
+           query-url     (if-not (:page options-map)
+                           (-> *url-base*
+                               (uri/param "q" encoded-query)
+                               (uri/param "auth" auth))
+                           (-> *url-base*
+                               (uri/param "q" encoded-query)
+                               (uri/param "auth" auth)
+                               (uri/param "p" (:page options-map))))
+           
+           wait-for      (if @*last-request*
+                           (- (coerce/to-long
+                               (time/plus @*last-request*
+                                          (time/seconds *delay-secs*)))
+                              (coerce/to-long (time/now))) 0)
            
            result        (do
-                           (Thread/sleep
-                            (if @*last-request*
-                              (time/minus
-                               (time/plus @*last-request*
-                                          (time/seconds 10))
-                               cur-time)
-                              0))
+                           (when (< 0 wait-for)
+                             (Thread/sleep wait-for))
+                           (swap! *last-request* (fn [x] (time/now)))
                            (-> query-url client/get :body))]
 
        (if (:json options-map)
