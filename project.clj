73149054065861c91ebf61f85ae7e69708a31828
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-16 01:32:10 -0500

    page 1 is processed

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-http "0.7.9"]
+  :dependencies [[cheshire "5.3.1"]
+                 [clj-http "0.7.9"]
                  [clj-time "0.6.0"]
                  [org.bovinegenius/exploding-fish "0.3.4"]
                  [org.clojure/clojure "1.5.1"]])
--- src/clj_blekko/core.clj
+++ src/clj_blekko/core.clj
@@ -1,8 +1,8 @@
 (ns clj-blekko.core
-  (:require [clj-time.core :as time]
+  (:require [cheshire.core :as json]
+            [clj-time.core :as time]
             [clj-http.client :as client]
-            (org.bovinegenius [exploding-fish :as uri]))
-  (:import [java.net URLEncoder]))
+            (org.bovinegenius [exploding-fish :as uri])))
 
 (def *url-base* "http://blekko.com/ws/")
 
@@ -14,9 +14,20 @@
 
 (defn run-query
   "Rate limited to 1 query per second"
-  [q auth]
-  (let [encoded-query (URLEncoder/encode q)
-        query-url     (-> *url-base*
-                          (uri/param "q" encoded-query)
-                          (uri/param "auth" auth))]
-    (client/get query-url)))
+  ([q auth]
+     (run-query q auth nil))
+
+  ([q auth & options]
+     (let [options-map   (into {} [(into [] options)])
+
+           encoded-query (if (:json options-map)
+                           (str q " /json /ps=100")
+                           (str q " /ps=100"))
+           query-url     (-> *url-base*
+                             (uri/param "q" encoded-query)
+                             (uri/param "auth" auth))
+
+           result        (-> query-url client/get :body)]
+       (if (:json options-map)
+         (json/parse-string result true)
+         result))))
