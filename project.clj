931fa92dc6987855c460fd4c2ba962c4641664ea
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-18 00:32:37 -0500

    added descr to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj_blekko "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj_blekko "0.1.0"
+  :description "Clojure implementation of the blekko API"
+  :url "http://github.com/shriphani/clj_blekko"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.3.1"]
